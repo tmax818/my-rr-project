93f8192a7a11560c8d1db2cdf75000ac1d9fdbf3
tmax818 <tylermaxwell661@gmail.com>
2020-10-06 13:35:22 -0700

    finish w/o refactor

--- src/Directory.js
+++ src/Directory.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
 
 class Directory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {
+    weeks: [
+      {
+        id: 0,
+        name: "Week One",
+        image: "assets/images/week-one.png",
+        description: "You know, girl!",
+      },
+      {
+        id: 1,
+        name: "Week Two",
+        image: "assets/images/week-two.png",
+        description: "What do we do this week again?",
+      },
+      {
+        id: 2,
+        name: "Week Three",
+        image: "assets/images/week-three.png",
+        description: "What do we do this week again?",
+      },
+      {
+        id: 3,
+        name: "Week Four",
+        image: "assets/images/week-four.png",
+        description: "What do we do this week again?",
+      },
+    ],
+  };
 
   render() {
+    const directory = this.state.weeks.map((week) => {
+      return (
+        <div key={week.id} className="col">
+          <img src={week.image} alt={week.name} />
+          <h2>{week.name}</h2>
+          <p>{week.description}</p>
+        </div>
+      );
+    });
+
     return (
-      <div>
-        <img src="assets/images/test-image.png" alt="" />
+      <div className="container">
+        <div className="row">{directory}</div>
       </div>
     );
   }
