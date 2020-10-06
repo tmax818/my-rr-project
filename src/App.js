70ea894f3563c16933387768024c6388c5dc6f18
tmax818 <tylermaxwell661@gmail.com>
2020-10-06 13:24:14 -0700

    start

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
 
+import Directory from "./Directory";
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +13,7 @@ class App extends Component {
             <NavbarBrand href="/">Tyler's React Class Project</NavbarBrand>
           </div>
         </Navbar>
+        <Directory />
       </div>
     );
   }
---
+++ src/Directory.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Directory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <img src="assets/images/test-image.png" alt="" />
+      </div>
+    );
+  }
+}
+
+export default Directory;
